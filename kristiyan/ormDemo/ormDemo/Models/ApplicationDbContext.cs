4e0eb328385bbe0ed319f9f8d72cdf2d88a41544
Kristiyan Denkov <kristiyan.denkov@stemo.bg>
2024-02-12 13:52:57 +0200

    Adding CodeFirst Exhample

---
+++ kristiyan/ormDemo/ormDemo/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ormDemo.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Integrated security=true;Database=CodeFirstDemo2");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
