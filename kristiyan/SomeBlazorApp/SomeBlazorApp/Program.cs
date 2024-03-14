bc5fb23971e156df8e665ff773f735e5aff98020
Kristiyan Denkov <kristiyan.denkov@stemo.bg>
2024-03-14 17:05:26 +0200

    Adding Blazor App ( for real this time xD )

---
+++ kristiyan/SomeBlazorApp/SomeBlazorApp/Program.cs
@@ -0,0 +1,39 @@
+using SomeBlazorApp.Components;
+using SomeBlazorApp.Services;
+
+namespace SomeBlazorApp;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        builder.Services.AddSingleton<AppStateService>();
+
+        
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
---
+++ kristiyan/SomeBlazorApp/SomeBlazorApp/Services/AppStateService.cs
@@ -0,0 +1,13 @@
+﻿namespace SomeBlazorApp.Services;
+
+public class AppStateService
+{
+    public int currentCount { get; set; } = 0;
+
+    public int upgrades { get; set; } = 0;
+
+    public int incrementAmount { get; set; } = 1;
+
+    public int price { get; set; } = 10;
+    public bool hasMoney { get; set; } = true;
+}
