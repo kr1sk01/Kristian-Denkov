e1811e2329dd08c5e5fd38035634924a3f2f1eea
teodor.t.georgiev <teodor.t.georgiev@stemo.bg>
2024-03-22 19:18:10 +0200

    Fixed TeamType

---
+++ ChampionshipApp/ChampionshipApp/Migrations/20240322171334_AlteredTeamType.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChampionshipApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlteredTeamType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "TeamTypes");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "TeamTypes");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "TeamTypes");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedOn",
+                table: "TeamTypes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeamSize",
+                table: "TeamTypes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeamSize",
+                table: "TeamTypes");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "TeamTypes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "TeamTypes",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "TeamTypes",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedOn",
+                table: "TeamTypes",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+    }
+}
--- ChampionshipApp/ChampionshipApp/Models/TeamType.cs
+++ ChampionshipApp/ChampionshipApp/Models/TeamType.cs
@@ -4,16 +4,12 @@ namespace ChampionshipApp.Models
 {
     public class TeamType
     {
-        [Required]
+        [Key]
         public int Id { get; set; }
         [Required]
         [StringLength(255)]
         public string? Name { get; set; }
         [Required]
-        public string? CreatedBy { get; set; }
-        [Required]
-        public DateTime? CreatedOn { get; set; }
-        public string? ModifiedBy { get; set; }
-        public DateTime? ModifiedOn { get; set; }
+        public int TeamSize { get; set; }
     }
 }
