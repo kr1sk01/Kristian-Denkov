7393fe6f4e15d9f804f86336731474b35cc59ca3
Kristiyan Denkov <kristiyan.denkov@stemo.bg>
2024-04-15 16:52:40 +0300

    Added Login Form

--- ChampionshipMaster/ChampionshipMaster.SHARED/ViewModels/LoginViewModel.cs
+++ ChampionshipMaster/ChampionshipMaster.SHARED/ViewModels/LoginViewModel.cs
@@ -8,9 +8,11 @@ namespace ChampionshipMaster.SHARED.ViewModels
 {
     public class LoginViewModel
     {
-        [Required]
-        public required string Email { get; set; }
-        [Required]
-        public required string Password { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string? Password { get; set; }
     }
 }
--- ChampionshipMaster/ChampionshipMaster.Web/ChampionshipMaster.Web/Program.cs
+++ ChampionshipMaster/ChampionshipMaster.Web/ChampionshipMaster.Web/Program.cs
@@ -11,7 +11,7 @@ public class Program
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents()
             .AddInteractiveWebAssemblyComponents();
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:50397/") });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:64687/") });
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
