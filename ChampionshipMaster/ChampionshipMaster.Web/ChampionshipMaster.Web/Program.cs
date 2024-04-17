4f5842c447a4fc8c45a28effb42a4fd25ab0a94e
Teodor Georgiev <sitikom2007@gmail.com>
2024-04-18 02:19:31 +0300

    Cleaned up AdminPage

--- ChampionshipMaster/ChampionshipMaster.Web/ChampionshipMaster.Web/Program.cs
+++ ChampionshipMaster/ChampionshipMaster.Web/ChampionshipMaster.Web/Program.cs
@@ -15,7 +15,7 @@ public class Program
             .AddInteractiveWebAssemblyComponents();
 
         builder.Services.AddRadzenComponents();
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:54241") });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:56664") });
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
