26f5fc9a2e7c15467bbf0d77957843d3d0577f2c
teodor.t.georgiev <teodor.t.georgiev@stemo.bg>
2024-04-18 10:33:25 +0300

    Fixed Deleting new item bug

--- ChampionshipMaster/ChampionshipMaster.Web/ChampionshipMaster.Web/Program.cs
+++ ChampionshipMaster/ChampionshipMaster.Web/ChampionshipMaster.Web/Program.cs
@@ -15,7 +15,7 @@ public class Program
             .AddInteractiveWebAssemblyComponents();
 
         builder.Services.AddRadzenComponents();
-        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:56664") });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:50397") });
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
