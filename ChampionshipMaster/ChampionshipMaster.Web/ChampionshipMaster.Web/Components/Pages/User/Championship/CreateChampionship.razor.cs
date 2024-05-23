081cd4270750660e6c25a8e152345234f72a63e5
Kristiyan Denkov <kristiyan.denkov@stemo.bg>
2024-05-23 09:38:55 +0300

    Fixed radio buttons possition in EditGame and created empty create champion page

---
+++ ChampionshipMaster/ChampionshipMaster.Web/ChampionshipMaster.Web/Components/Pages/User/Championship/CreateChampionship.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace ChampionshipMaster.Web.Components.Pages.User.Championship;
+
+public partial class CreateChampionship : ComponentBase
+{
+    [Inject] NavigationManager nManager { get; set; } = default!;
+    [Inject] Radzen.DialogService dialogService { get; set; } = default!;
+    [Inject] IConfiguration configuration { get; set; } = default!;
+    [Inject] ITokenService tokenService { get; set; } = default!;
+    [Inject] IHttpClientFactory httpClient { get; set; } = default!;
+    [Inject] IWebHostEnvironment Environment { get; set; } = default!;
+
+    [Inject] ProtectedLocalStorage _localStorage { get; set; } = default!;
+    [Inject] NavigationManager NavigationManager { get; set; } = default!;
+    [Inject] ContextMenuService ContextMenuService { get; set; } = default!;
+    [Inject] DialogService DialogService { get; set; } = default!;
+
+
+
+}
