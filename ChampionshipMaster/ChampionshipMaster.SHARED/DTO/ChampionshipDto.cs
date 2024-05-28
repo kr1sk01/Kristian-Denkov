d40093f0c690ea7791895fa91acfbba1ad6bafc1
Kristian Denkov <cross.2001@outlook.com>
2024-05-28 10:40:46 +0300

    Added DTOs & create game fix

--- ChampionshipMaster/ChampionshipMaster.SHARED/DTO/ChampionshipDto.cs
+++ ChampionshipMaster/ChampionshipMaster.SHARED/DTO/ChampionshipDto.cs
@@ -4,26 +4,18 @@
     {
 
         public int Id { get; set; }
-
-
         public string? Name { get; set; }
-
         public byte[]? Logo { get; set; }
-
         public string? ChampionshipTypeName { get; set; }
-
         public string? ChampionshipStatusName { get; set; }
-
         public string? GameTypeName { get; set; }
-
+        public virtual ChampionshipTypeDto? ChampionshipType { get; set; }
+        public virtual ChampionshipStatusDto? ChampionshipStatus { get; set; }
+        public virtual GameTypeDto? GameType { get; set; }
         public string? WinnerName { get; set; }
-
         public DateTime? LotDate { get; set; }
-
         public DateTime? Date { get; set; }
-
         public virtual ICollection<GameDto> Games { get; set; } = new HashSet<GameDto>();
-
         public virtual ICollection<TeamDto> Teams { get; set; } = new HashSet<TeamDto>();
     }
 }
---
+++ ChampionshipMaster/ChampionshipMaster.SHARED/DTO/ChampionshipStatusDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChampionshipMaster.SHARED.DTO
+{
+    public class ChampionshipStatusDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
