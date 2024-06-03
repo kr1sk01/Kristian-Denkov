a1d01a81ffc63558d2401c8d598f2e904229f8d2
kr1sk01 <cross.2001@outlook.com>
2024-06-03 17:43:37 +0300

    Added button needed furter styling ( teams page )

---
+++ ChampionshipMaster/ChampionshipMaster.Web/ChampionshipMaster.Web/Components/Pages/User/Team/TeamHistory.razor.cs
@@ -0,0 +1,22 @@
+﻿namespace ChampionshipMaster.Web.Components.Pages.User.Team
+{
+    public partial class TeamHistory : ComponentBase
+    {
+        [Inject]
+        ITokenService tokenService { get; set; } = default!;
+        [Inject]
+        IHttpClientFactory httpClient { get; set; } = default!;
+        [Inject]
+        INotifier notifier { get; set; } = default!;
+        [Inject]
+        IConfiguration configuration { get; set; } = default!;
+        [Inject]
+        IImageService imageService { get; set; } = default!;
+        [Inject]
+        NavigationManager NavigationManager { get; set; } = default!;
+
+        [Parameter] public string id { get; set; }
+
+
+    }
+}
