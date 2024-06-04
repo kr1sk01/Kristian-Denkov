961d9361f9134b26091daee933894e5445723782
Kristian Denkov <cross.2001@outlook.com>
2024-06-05 00:36:25 +0300

    Fully Functional team history page

--- ChampionshipMaster/ChampionshipMaster.Web/ChampionshipMaster.Web/Components/Pages/User/Team/TeamHistory.razor.cs
+++ ChampionshipMaster/ChampionshipMaster.Web/ChampionshipMaster.Web/Components/Pages/User/Team/TeamHistory.razor.cs
@@ -15,37 +15,44 @@
         [Inject]
         NavigationManager NavigationManager { get; set; } = default!;
 
-        [Parameter] public string id { get; set; }
+        [Parameter] public string id { get; set; } = default!;
+
+        public TeamDto currentTeam = new();
+
         public List<GameDto>? games;
 
         public bool isAdmin = false;
         public bool isLogged = false;
         class DataItem
         {
-            public string Category { get; set; }
-            public double Count { get; set; }
-            public string Color { get; set; }
+            public string Category { get; set; } = default!;
+            public double Count { get; set; } = default!;
+            public string Color { get; set; } = default!;
         }
 
         DataItem[] data = new DataItem[] {
-        new DataItem
-        {
-            Category = "Wins",
-            Count = 21,
-            Color = "#00FF00"
-        },
-        new DataItem
-        {
-            Category = "Loses",
-            Count = 56,
-            Color = "#FF0000"
-        },
-    };
+                    new DataItem
+                    {
+                        Category = "Wins",
+                        Count = 0,
+                        Color = "#00FF00"
+                    },
+                    new DataItem
+                    {
+                        Category = "Loses",
+                        Count = 0,
+                        Color = "#FF0000"
+                    },
+                };
+
         string[] fills = { "#00FF00", "#FF0000" }; // Red, Green, Blue
+
         public int wins { get; set; }
         public int loses { get; set; }
         public double winratio { get; set; }
-        public int gamesPlayed { get; set; } 
+        public int gamesPlayed { get; set; }
+
+        public double winrationPercentage { get; set; }
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
@@ -60,9 +67,31 @@
                 else { NavigationManager.NavigateTo("/login"); }
 
                 using HttpClient client = httpClient.CreateClient(configuration["ClientName"]!);
+
+                var team = await client.GetFromJsonAsync<TeamDto>($"/api/Teams/{id}");
+                if (team != null)
+                    currentTeam = team;
                 var test = await client.GetFromJsonAsync<List<GameDto>>($"/api/Teams/Game_History/{id}");
-                if(test != null)               
+                if (test != null)
                     games = test;
+
+                if (games != null)
+                    foreach (var item in games)
+                    {
+                        if (item.GameStatusName == "Finished" && item.Winner != null)
+                        {
+                            if (item.Winner.Id == int.Parse(id)) wins++;
+                            if (item.BlueTeam!.Id == int.Parse(id) || item.RedTeam!.Id == int.Parse(id)) gamesPlayed++;
+
+                            loses = gamesPlayed - wins;
+                            data[0].Count = wins;
+                            data[1].Count = loses;
+                        }
+                        if (gamesPlayed > 0)
+                        {
+                            winrationPercentage = (double)(((double)wins / (double)gamesPlayed) * 100);
+                        }
+                    }
 
 
                 ;
